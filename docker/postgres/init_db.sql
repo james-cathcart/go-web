3e5c27055e3b1b0092df7af00b6fef5d1d71521c
James Cathcart <cathcart.jm@gmail.com>
2020-11-23 11:55:15 -0700

    Completed:  - Added Dockerfile, docker-compose.yml, and init_db.sql database initialization scripts  - Confirmed that database populates based on the init_db.sql scripts
    In Progress:
    
    Notes:

---
+++ docker/postgres/init_db.sql
@@ -0,0 +1,35 @@
+create schema if not exists chitchat;
+
+create table chitchat.users (
+  id         serial primary key,
+  uuid       varchar(64) not null unique,
+  name       varchar(255),
+  email      varchar(255) not null unique,
+  password   varchar(255) not null,
+  created_at timestamp not null
+);
+
+create table chitchat.sessions (
+  id         serial primary key,
+  uuid       varchar(64) not null unique,
+  email      varchar(255),
+  user_id    integer references users(id),
+  created_at timestamp not null
+);
+
+create table chitchat.threads (
+  id         serial primary key,
+  uuid       varchar(64) not null unique,
+  topic      text,
+  user_id    integer references users(id),
+  created_at timestamp not null
+);
+
+create table chitchat.posts (
+  id         serial primary key,
+  uuid       varchar(64) not null unique,
+  body       text,
+  user_id    integer references users(id),
+  thread_id  integer references threads(id),
+  created_at timestamp not null
+);
